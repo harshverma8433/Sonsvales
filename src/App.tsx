c40e7359518b08e2fe0f07bc4820dd5ba2adf878
harsh verma <harshverma8433@harshs-MacBook-Air.local>
2025-07-05 19:57:11 +0530

    app changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Index from "./pages/Index";
-import TrustedSection from "@/components/TrustedSection/TrustedSection.tsx";
-import ServicesSection from '@/Components/ServicesSection/ServicesSection.tsx';
-import WhyChooseUs from '@/Components/WhyChooseUs/WhyChooseUs.tsx';
-import Differentiators from '@/Components/Differentiators/Differentiators.tsx';
-import TechnologiesSection from '@/Components/TechnologiesSection/TechnologiesSection.tsx';
-import ProcessSteps from '@/Components/ProcessSteps/ProcessSteps.tsx';
-import FAQSection from '@/Components/FAQs/FAQSection.tsx';
-import Footer from '@/Components/Footer/Footer.tsx';
+import TrustedSection from "../src/components/TrustedSection/TrustedSection";
+import ServicesSection from "../src/components/ServicesSection/ServicesSection";
+import WhyChooseUs from  '../src/components/WhyChooseUs/WhyChooseUs';
+import Differentiators from '../src/components/Differentiators/Differentiators';
+import TechnologiesSection from '../src/components/TechnologiesSection/TechnologiesSection';
+import ProcessSteps from '../src/components/ProcessSteps/ProcessSteps';
+import FAQSection from '../src/components/FAQs/FAQSection';
+import Footer from '../src/components/Footer/Footer';
 
 const App = () => (
 
